100e77a16368690e6bd99a068f874d1b989b0d22
Manikumar Garimella <gsvmk06@gmail.com>
2026-06-23 16:07:53 +0530

    Adding the polymorphing values part 2

--- Polymorphism/problem2.py
+++ Polymorphism/problem2.py
@@ -24,7 +24,7 @@ class Person:
         
 
 
-p1 = Person("Hari", 2500, "1990-06-15")
+p1 = Person("Hari", 2500, "2000-06-15")
 p2 = Person("Raj", 3000, "1996-03-20")
 
 # Menu-driven program
@@ -37,17 +37,19 @@ while True:
     choice = int(input("Enter your choice: "))
 
     if choice == 1:
+        p1.op ,p2.op = "salary","salary"
         if p1 > p2:
             print(f"{p1.name} is gains more than {p2.name} ")
         elif p1 < p2:
-            print(f"{p2.name} is gains less than {p1.name} ")
+            print(f"{p2.name} is gains more than {p1.name} ")
         else:
             print("Both have the same salary")
 
     elif choice == 2:
+        p1.op ,p2.op = "dob","dob"
         if p1 < p2:
             print(f"{p1.name} is older (DOB: {p1.dob})")
-        elif p1 < p2:
+        elif p1 > p2:
             print(f"{p2.name} is older (DOB: {p2.dob})")
         else:
             print("Both have the same DOB")
